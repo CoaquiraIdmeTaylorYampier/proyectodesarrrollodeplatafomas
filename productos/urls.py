81c0f17f0ffdabd70e4b05ce5bb9ab505336f375
Taylor <coaquiraidmetaylor@gmail.com>
2025-07-24 20:40:51 -0500

    aguegue el carrito mejorado

--- productos/urls.py
+++ productos/urls.py
@@ -7,9 +7,5 @@ urlpatterns = [
     path('sensores/', views.sensores, name='sensores'),
     path('mcu/', views.mcu, name='mcu'),
     path('accesorios/', views.accesorios, name='accesorios'),
-<<<<<<< HEAD
-=======
 
->>>>>>> 3d94c0261033b0def27954e1ddd3a47a93fc5769
-    
 ]
